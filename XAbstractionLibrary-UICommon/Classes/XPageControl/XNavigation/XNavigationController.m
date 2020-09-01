2023f486a08c643df8aacfd161b3ccebec6a980b
lanbiao <lanbiao@zhihuihutong.com>
2020-09-01 17:24:42 +0800

    Navigatgion的升级兼容

--- XAbstractionLibrary-UICommon/Classes/XPageControl/XNavigation/XNavigationController.m
+++ XAbstractionLibrary-UICommon/Classes/XPageControl/XNavigation/XNavigationController.m
@@ -85,6 +85,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.navigationBarHidden = YES;
 }
 
 - (void)didReceiveMemoryWarning {
